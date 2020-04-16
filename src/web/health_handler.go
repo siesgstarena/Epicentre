e0327071f9363dd27af2c696f2ce47729882b266
Swap76 <swapnil.satish17@siesgst.ac.in>
2020-04-16 23:41:03 +0530

    Health and Version API as per format

--- src/web/health_handler.go
+++ src/web/health_handler.go
@@ -2,13 +2,25 @@ package web
 
 import (
 	"fmt"
+	"time"
 	"github.com/gin-gonic/gin"
 )
+
+type health struct {
+    Name     		string `json:"name"`
+	Description     string `json:"description"`
+	Health 			string `json:"health"`
+	Timestamp       time.Time `json:"timestamp"`
+}
 
 // HeathHandler Sends info about health of API
 func HeathHandler(c *gin.Context)  {
 	fmt.Println("Inside Health Handler")
-	c.JSON(200, gin.H{
-		"health": "perfect",
-	})
+	info := new(health)
+	info.Name = "epicentre"
+	info.Description = "Cloud Monitoring and Alerting Tool built by SIESGSTarena Platform Team)"
+	info.Health = "UP"
+	loc, _ := time.LoadLocation("UTC")
+    info.Timestamp = time.Now().In(loc)
+	c.JSON(200, info)
 }
--- src/web/version_handler.go
+++ src/web/version_handler.go
@@ -2,13 +2,27 @@ package web
 
 import (
 	"fmt"
+	"runtime"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
+
+type version struct {
+    Name     		string `json:"name"`
+	Description     string `json:"description"`
+	Version 		string `json:"health"`
+	Timestamp       time.Time `json:"timestamp"`
+}
 
 // VersionHandler Sends info about version of API
 func VersionHandler(c *gin.Context)  {
 	fmt.Println("Inside Health Handler")
-	c.JSON(200, gin.H{
-		"version": "v2.0",
-	})
+	info := new(version)
+	info.Name = "epicentre"
+	info.Description = "Cloud Monitoring and Alerting Tool built by SIESGSTarena Platform Team)"
+	info.Version = runtime.Version()
+	loc, _ := time.LoadLocation("UTC")
+    info.Timestamp = time.Now().In(loc)
+	c.JSON(200, info)
 }
