2c2c037503eb89b8b8f9803a86e6491ce30f9363
Swap76 <swapnil.satish17@siesgst.ac.in>
2020-05-08 14:07:47 +0530

    Rules Added for Heroku

--- model/rule.go
+++ model/rule.go
@@ -4,20 +4,25 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 //Rules Type exported for use in API
 type Rules struct {
-	ID     			primitive.ObjectID 	`bson:"_id,omitempty"`
-	UserID			primitive.ObjectID 	`bson:"userid,omitempty"`
-	ProjectID   	primitive.ObjectID	`bson:"projectid,omitempty"`
-	HerokuAddons 	bool				`bson:"herokuaddons,omitempty"`
-	HerokuBuilds 	bool				`bson:"herokubuilds,omitempty"`
-	CreateBranch 	bool 				`bson:"createbranch,omitempty"`
-	DeleteBranch 	bool				`bson:"deletebranch,omitempty"`
-	PR 				bool 				`bson:"pr,omitempty"`
-	PrReview		bool 				`bson:"prreview,omitempty"`
-	Push			bool 				`bson:"push,omitempty"`
-	Release			bool 				`bson:"release,omitempty"`
-	Issues   		bool				`bson:"issues,omitempty"`
-	Forks			bool				`bson:"forks,omitempty"`
-	ProjectBoard	bool				`bson:"projectboard,omitempty"`	
-	ProjectCard		bool				`bson:"projectcard,omitempty"`	
-	ProjectColumn	bool				`bson:"projectcolumn,omitempty"`		
+	ID     					primitive.ObjectID 	`bson:"_id,omitempty"`
+	UserID					primitive.ObjectID 	`bson:"userid,omitempty"`
+	ProjectID   			primitive.ObjectID	`bson:"projectid,omitempty"`
+	HerokuAddonAttachment 	bool				`bson:"addon-attachment,omitempty"`
+	HerokuAddon 			bool				`bson:"addon,omitempty"`
+	HerokuApp				bool				`bson:"app,omitempty"`
+	HerokuBuild 			bool				`bson:"build,omitempty"`
+	HerokuCollaborator		bool				`bson:"collaborator,omitempty"`
+	HerokuDomain			bool				`bson:"domain,omitempty"`
+	HerokuDyno				bool				`bson:"dyno,omitempty"`
+	CreateBranch 			bool 				`bson:"createbranch,omitempty"`
+	DeleteBranch 			bool				`bson:"deletebranch,omitempty"`
+	PR 						bool 				`bson:"pr,omitempty"`
+	PrReview				bool 				`bson:"prreview,omitempty"`
+	Push					bool 				`bson:"push,omitempty"`
+	Release					bool 				`bson:"release,omitempty"`
+	Issues   				bool				`bson:"issues,omitempty"`
+	Forks					bool				`bson:"forks,omitempty"`
+	ProjectBoard			bool				`bson:"projectboard,omitempty"`	
+	ProjectCard				bool				`bson:"projectcard,omitempty"`	
+	ProjectColumn			bool				`bson:"projectcolumn,omitempty"`		
 }
