f4759895590e2a61c98096c43588ab834c4e34f3
Swap76 <swapnil.satish17@siesgst.ac.in>
2020-05-09 20:41:51 +0530

    Rule Model modifies to suit the github event calles

--- model/rule.go
+++ model/rule.go
@@ -14,15 +14,17 @@ type Rules struct {
 	HerokuCollaborator		bool				`bson:"collaborator,omitempty"`
 	HerokuDomain			bool				`bson:"domain,omitempty"`
 	HerokuDyno				bool				`bson:"dyno,omitempty"`
-	CreateBranch 			bool 				`bson:"createbranch,omitempty"`
-	DeleteBranch 			bool				`bson:"deletebranch,omitempty"`
-	PR 						bool 				`bson:"pr,omitempty"`
-	PrReview				bool 				`bson:"prreview,omitempty"`
+	Create					bool 				`bson:"create,omitempty"`
+	Delete		 			bool				`bson:"delete,omitempty"`
+	Deployment				bool				`bson:"deployment,omitempty"`
+	Issues   				bool				`bson:"issues,omitempty"`
+	IssueComment   			bool				`bson:"issue_comment,omitempty"`
+	PR 						bool 				`bson:"pull_request,omitempty"`
+	PrReview				bool 				`bson:"pull_request_review,omitempty"`
+	PrReviewComment			bool 				`bson:"pull_request_review_comment,omitempty"`
 	Push					bool 				`bson:"push,omitempty"`
 	Release					bool 				`bson:"release,omitempty"`
-	Issues   				bool				`bson:"issues,omitempty"`
-	Forks					bool				`bson:"forks,omitempty"`
-	ProjectBoard			bool				`bson:"projectboard,omitempty"`	
-	ProjectCard				bool				`bson:"projectcard,omitempty"`	
-	ProjectColumn			bool				`bson:"projectcolumn,omitempty"`		
+	Project					bool				`bson:"project,omitempty"`	
+	ProjectCard				bool				`bson:"project_card,omitempty"`	
+	ProjectColumn			bool				`bson:"project_column,omitempty"`		
 }
